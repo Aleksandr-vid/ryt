01dcb8754c9d081006624a14c7aa1b29222196a5
unknown <mister.videman@gmail.com>
2021-04-06 20:45:53 +0500

    список где не больше 10 элементов

---
+++ list_10.py
@@ -0,0 +1,20 @@
+#список где не больше 10 элементов
+
+class Top(list):
+    def __init__(self, b):
+        if len(b)>10:
+            raise ValueError('>10')
+        else:
+            super().__init__(b)
+
+    def Qur(self,b):
+        if len(self)==10:
+            raise ValueError('>10')
+        else:
+            super().Qur(b)
+
+h=Top([1,2,3,4,5,6,7,8,9,10])
+print(h)
+
+
+
