64d683051daa9dec7ce798d8d170049ba4060285
unknown <mister.videman@gmail.com>
2021-04-09 20:12:36 +0500

    изменение 2

--- list_10.py
+++ list_10.py
@@ -2,14 +2,20 @@
 
 class Top(list):
     def __init__(self, b):
-        if len(b)==10:
+        if len(b)>10:
             raise ValueError('>10')
         else:
             super().__init__(b)
+    def append(self,x):
+        if len(self)==10:
+            raise ValueError('>10')
+        else:
+            super().append()
 
-h=Top([1,2,3,4,5,6,7,8,9,10,])+[11]
-
+h=Top([1,2,3,4,5,6,7,8,9,10]+[11])
 print(h)
+
+
 
 
 
